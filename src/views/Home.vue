7e396f0cc4d198d58035f253904d301ab343d077
fxpby <wx_d7871bd34abb4abe94643d6ca73e0252@git.code.tencent.com>
2019-11-19 16:38:46 +0800

    构建项目、配置全局样式及路由

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <!-- msg内容传入helloworld组件 -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <h2>{{ mes }}</h2> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      mes: "this is home page"
+    };
+  }
+};
+</script>
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    aha
+    <el-row>
+      <el-button>默认按钮</el-button>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #ccc;
+}
+</style>
